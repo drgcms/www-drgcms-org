d27074842f4d1111026a7d0be8c0c2d0fad736f7
Damjan Rems <damjan.rems@gmail.com>
2015-03-09 18:11:27 +0100

    Making things preatier.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,10 @@ module ApplicationHelper
 
 #####################################################################
 # Return DcPiece document with name 'social sites' which should hold 
-# data for rendering social sites links on browser output.
+# data for rendering social sites links on browser output. 
+# 
+# It is up to developer how to implement social sites links so implement
+# it in yours application helper.
 #####################################################################
 def social_sites
   social = DcPiece.find_by(name: 'social sites')
