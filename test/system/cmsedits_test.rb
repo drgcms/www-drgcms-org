527fdf2a94f6f8290a86a5c9db419979d80df8ff
Damjan Rems <damjan.rems@gmail.com>
2018-02-15 22:00:16 +0100

    New tests added.

--- test/system/cmsedits_test.rb
+++ test/system/cmsedits_test.rb
@@ -3,14 +3,14 @@ require "application_system_test_case"
 class CmseditsTest < ApplicationSystemTestCase
 def login
   visit '/cms/login'
-  assert_selector "h2", text: "CMS login"
+  assert_selector "h2", text: "CMS"
   fill_in 'record_username', with: 'admin'
   fill_in 'record_password', with: 'secret'
   find('input[type="submit"]').click
   assert_selector "#cmsedit-div"
 end
 
-=begin
+#=begin
   test "browse_and_update_collection" do
     login
     click_link 'CMS'
@@ -61,7 +61,7 @@ end
     end     
         sleep 2
   end
-=end  
+#=end  
   test "cmsedit_tests" do
     exit
     
@@ -107,10 +107,12 @@ end
   end
   
    test "cmsedit_form_fields_test" do
+     site = DcSite.find_by(name: 'www.mysite.com')
      login
      visit "/cms/new?formname=demo_form&table=dc_memory"
      assert_selector ".dc-form-frame"
-# textfield field11
+     
+  # textfield field11
      value = 'Value field11'
      fill_in 'record_field11', with: value
      assert_equal find_field('record_field11').value, value
@@ -122,7 +124,63 @@ end
      within("#td_record_field3") {
        assert find(:xpath, "(//td[text()='ReadOnly field 1'])")
      }
+# textfield field6
+     assert_empty find_field('record_field6').value
+     value = 'Value field6'
+     fill_in 'record_field6', with: value
+     assert_equal find_field('record_field6').value, value  
      
+# text_with_select field7
+     assert_empty find_field('record_field7').value
+     find_field('field7_').find(:option, 'two').select_option
+     assert_equal find_field('record_field7').value, 'two'
+  
+# TAB SELECT FIELDS
+     find(:xpath, "(//li[text()='SELECT FIELDS'])").click
+#     assert_equal find_field('record_field20').value, '0'
+#     find_field('record_field20').click
+#     assert_equal find_field('record_field20').value, '0'
+      assert page.has_no_checked_field?('record_field20')
+      find_field('record_field20').click
+      assert page.has_checked_field?('record_field20')
+# select field21
+     assert_equal find_field('record_field21').value, 'Yes'
+     find_field('record_field21').find(:option, 'No').select_option
+     assert_equal find_field('record_field21').value, 'No'
+     
+# select field22
+     assert_empty find_field('record_field22').value
+     find_field('record_field22').find(:option, 'Sent').select_option
+     assert_equal find_field('record_field22').value, '3'
+# select field23
+     assert_equal find_field('record_field23').value, '1'
+     find_field('record_field23').find(:option, 'Four').select_option
+     assert_equal find_field('record_field23').value, '4'
+
+# select field24
+     assert_empty find_field('record_field24').value
+     find_field('record_field24').find(:option, 'www.mysite.com').select_option
+     assert_equal find_field('record_field24').value, site.id.to_s
+
+# select field25
+     assert_empty find_field('record_field25').value
+     find_field('record_field25').find(:option, 'six').select_option
+     assert_equal find_field('record_field25').value, '6'
+
+  # TAB SELECT CONTINUES
+     find(:xpath, "(//li[text()='SELECT CONTINUES'])").click
+     within("#td_record_field31") {
+#       within(:xpath, "(select)") {
+#         find(:xpath, "(//option[text()='www.mysite.com'])").click
+#       }
+     }
+
+  #
+     find_field('record__field32').click
+     fill_in 'record__field32', with: 'my'
+     find(:xpath, "(//div[text()='www.mysite.com'])").click
+     assert_equal find_field('record__field32').value, 'www.mysite.com'
+
    end
 
 end
