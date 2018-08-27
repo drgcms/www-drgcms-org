050e985644102b2977d875349e1cabffb2d86604
Damjan Rems <damjan.rems@gmail.com>
2018-08-27 15:22:37 +0200

    Blog path redone.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ WwwDrgcms::Application.routes.draw do
   root :to => "portal#page"
 #  get 'home' => 'portal#page'
   
-#  get '/blog/:name/:link' => 'dc_main#page', :defaults => { path: 'blog' }
-#  get '/blog/:name' => 'dc_main#page', :defaults => { path: 'blog', link: 'all' }
- # get '/news/:link' => 'dc_main#page', :defaults => { path: 'news' }
-  
   get '/plugins' => 'dc_main#page', :defaults => { path: 'plugins' }
   get '/plugins/search' => 'plugins#search'
   get '/dc_test' => 'dc_test#index' 
